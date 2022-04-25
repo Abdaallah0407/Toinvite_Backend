075608ca8d4ab040fd9fe95a4cf954ae6fc5a550
Abdaallah0407 <adilettulebaev@gmail.com>
2022-04-25 17:41:42 +0600

    allow

--- toinvite_core/settings.py
+++ toinvite_core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9djb(=$ehz0kpermynnd9k=1-7(k3*0af%$jcs0adk(r6vk@jo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['164.92.245.139', 'localhost', '127.0.0.1',]
 
 
 # Application definition
