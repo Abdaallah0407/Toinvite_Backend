1eac348c36db8d5fab0622beb3d544c563ce0dea
Altynai <abylgazievaalt@gmail.com>
2022-06-07 12:00:39 +0600

    guests list

--- src/events/serializers.py
+++ src/events/serializers.py
@@ -3,6 +3,13 @@ from django.db.models import fields
 from rest_framework import serializers
 from .models import *
 from src.accounts.serializers import UserDetailSerializer
+from ..guests.models import GuestsList
+
+
+class GuestListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GuestsList
+        fields = ('full_name', 'phone_number', 'status')
 
 
 class EventsListSerializer(serializers.ModelSerializer):
@@ -17,6 +24,10 @@ class EventsTitleSerializer(serializers.ModelSerializer):
 
 
 class EventsDetailSerializer(serializers.ModelSerializer):
+    guests_list = serializers.SerializerMethodField()
+    accepted = serializers.SerializerMethodField()
+    declined = serializers.SerializerMethodField()
+    undecided = serializers.SerializerMethodField()
     categories = serializers.SlugRelatedField(
         slug_field="title", read_only=True)
     user = serializers.SlugRelatedField(slug_field="username", read_only=True)
@@ -24,6 +35,19 @@ class EventsDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Events
         fields = "__all__"
+
+    def get_guests_list(self, obj):
+        data = GuestListSerializer(obj.guestslist_set.all().order_by('full_name'), many=True).data
+        return data
+
+    def get_accepted(self, obj):
+        return obj.guestslist_set.filter(status=True).count()
+
+    def get_declined(self, obj):
+        return obj.guestslist_set.filter(status=False).count()
+
+    def get_undecided(self, obj):
+        return obj.guestslist_set.filter(status=None).count()
 
 
 class AdUnpublishedSerializer(serializers.ModelSerializer):
